077fc44092a5ff8ed8f782c5d32a370bcf18b559
Ajman Mocsana <ajman.mocsana@g.msuiit.edu.ph>
2025-04-20 23:33:15 +0800

    updated database manager

--- Database_Manager/database.py
+++ Database_Manager/database.py
@@ -1,29 +1,20 @@
-import mysql.connector
+import pymysql
 
-def connect_database():
-    connection = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'eggyisback0525',
-            database = 'ssis_database'
-    )
-    return connection
-        
+class DatabaseManager:
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 3306
+        self.user = "root"
+        self.password = "eggyisback0525"
+        self.database = "ssis_database"
+        self.cursor = None
 
-    '''def fetch_all(self, table):
-        self.cursor.execute(f"SELECT * FROM {table}")
-        return self.cursor.fetchall()
-
-    def insert_student(self, studentName, programCode):
-        self.cursor.execute("INSERT INTO students (studentName, programCode) VALUES (%s, %s)", (studentName, programCode))
-        self.conn.commit()
-
-    def delete_student(self, studentID):
-        self.cursor.execute("DELETE FROM students WHERE student_id = %s", (studentID,))
-        self.conn.commit()
-
-    def close(self):
-        self.cursor.close()
-        self.conn.close()
-'''
-
+    def connect_database(self):
+        self.connection = pymysql.connect(
+            host=self.host,
+            user=self.user,
+            port=self.port,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
