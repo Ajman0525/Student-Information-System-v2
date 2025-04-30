ef5666a805595a5e28fe1de9f80b0c3a953dcceb
Ajman Mocsana <ajman.mocsana@g.msuiit.edu.ph>
2025-04-30 21:32:50 +0800

    updated main driver

--- main.py
+++ main.py
@@ -2,22 +2,21 @@
 #                          MySQL STUDENT INFORMATION SYSTEM                         #                 
 #                                                                                   #
 #  Made by: Ajman L. Mocsana                                                        #
-#  Date finished: April 21, 2025                                                    #
-#  database ip address: 192.168.1.3                                                 #
+#  Date finished: April 30, 2025                                                    #
+#                                                                                   #
 #####################################################################################
 
 
 import sys
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 from UI.display import Display
 
 def main():
     app = QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Display()
-    ui.setupUi(MainWindow)
-
+    UI = Display()
+    
     app.setStyleSheet("""
         QMessageBox {
             background-color: #fce090; 
@@ -44,7 +43,9 @@ def main():
             }
     """)
 
-    MainWindow.show()
+
+
+    UI.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
